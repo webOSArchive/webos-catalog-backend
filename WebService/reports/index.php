07ce6393ef50bcb5317db74914f8bfead344485c
codepoet80 <jonathan.m.wise@gmail.com>
2022-11-02 17:29:36 -0400

    tweak css

--- WebService/reports/index.php
+++ WebService/reports/index.php
@@ -9,7 +9,7 @@ $mimeType = "text/html";
             const downloadReport = <?php include('getdownloaddata.php'); ?>;
             const updateReport = <?php include('getupdatedata.php'); ?>;
         </script>
-        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="reports.css">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.9.1/chart.min.js" integrity="sha512-ElRFoEQdI5Ht6kZvyzXhYG9NqjtkmlkfYk0wr6wHxU9JEHakS7UJZNeml5ALk+8IKlU6jDgMabC3vkumRokgJA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
         <script src="reports.js"></script>

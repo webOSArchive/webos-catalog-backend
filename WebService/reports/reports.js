d0016297fd6cf0c9bc93b7b25921738c184fb6cf
codepoet80 <jonathan.m.wise@gmail.com>
2022-11-02 17:07:27 -0400

    clean-up

---
+++ WebService/reports/reports.js
@@ -0,0 +1,221 @@
+const CHART_COLORS = {
+    blue: 'rgb(0,48,143)',
+    purple: 'rgb(153, 102, 255)',
+    dimred: 'rgb(255, 99, 132)',
+    orange: 'rgb(255,103,0)',
+    yellow: 'rgb(255, 205, 86)',
+    dimgreen: 'rgb(75, 192, 192)',
+    brightred: 'rgb(211,33,45)',
+    grey: 'rgb(201, 203, 207)',
+    teal: 'rgb(77,166,255)',
+    green: 'rgb(102,255,0)',
+    lightblue: 'rgb(124,185,232)'
+};
+
+function renderReports() {
+    //Overall Stats
+    var statsDiv = document.getElementById("stats");
+    statsDiv.innerHTML += "<p><b>Report Range:</b><br/>" + downloadReport.firstDate + " - " + downloadReport.lastDate + "</p>";
+    statsDiv.innerHTML += "<p><b>Total Downloads:</b> " + downloadReport.totalDownloads + "</p>";
+    statsDiv.innerHTML += "<p><b>Total Update Checks:</b> " + updateReport.totalChecks + "</p>";
+    statsDiv.innerHTML += "<p><b>Unique Device Count:</b> " + updateReport.uniqueDevices + "</p>";
+
+    //App Download Stats
+    var appLabels = [];
+    var appTotals = [];
+    for (let key in downloadReport.topApps) {
+        if (downloadReport.topApps.hasOwnProperty(key)) {
+            //console.log(key, downloadReport.topApps[key]);
+            appLabels.push(downloadReport.topApps[key].appName);
+            appTotals.push(downloadReport.topApps[key].count);
+        }
+    }
+    var ctx = document.getElementById('appsChart');
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        options: {
+            responsive: true,
+            indexAxis: 'y',
+            plugins: {
+                title: {
+                    display: true,
+                    text: 'Most Downloaded Apps'
+                },
+                legend: {
+                    display: false
+                }
+            },
+        },
+        data: {
+        labels: appLabels,
+        datasets: [{ 
+            label: 'Downloads',
+            data: appTotals,
+            backgroundColor: Object.values(CHART_COLORS),
+            fill: true,
+            }]
+        },
+    });
+
+    //Download Device Stats
+    var downloaderLabels = [];
+    var downloaderTotals = [];
+    for (let key in downloadReport.topClients) {
+        if (downloadReport.topClients.hasOwnProperty(key)) {
+            //console.log(key, downloadReport.topClients[key]);
+            downloaderLabels.push(downloadReport.topClients[key].clientString);
+            downloaderTotals.push(downloadReport.topClients[key].count);
+        }
+    }
+    var ctx = document.getElementById('downloaderChart');
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        options: {
+            responsive: true,
+            plugins: {
+                title: {
+                    display: true,
+                    text: 'Most Common Downloaders'
+                },
+                legend: {
+                    display: false
+                }
+            },
+        },
+        data: {
+        labels: downloaderLabels,
+        datasets: [{ 
+            label: 'Downloads',
+            data: downloaderTotals,
+            backgroundColor: Object.values(CHART_COLORS),
+            fill: true,
+            }]
+        },
+    });
+
+    //App Update Stats
+    var updateLabels = [];
+    var updateTotals = [];
+    var updateUniques = [];
+    for (let key in updateReport.topApps) {
+        if (updateReport.topApps.hasOwnProperty(key)) {
+            //console.log(key, updateReport.topApps[key]);
+            updateLabels.push(updateReport.topApps[key].appName);
+            updateTotals.push(updateReport.topApps[key].count);
+            updateUniques.push(updateReport.topApps[key].uniqueDevices);
+        }
+    }
+    var ctx = document.getElementById('usageChart');
+    var myChart = new Chart(ctx, {
+        type: 'pie',
+        options: {
+            responsive: true,
+            plugins: {
+                legend: {
+                    position: 'top',
+                },
+                title: {
+                    display: true,
+                    text: 'Most Active Apps'
+                }
+            }
+        },
+        data: {
+        labels: updateLabels,
+        datasets: [{ 
+            data: updateTotals,
+            backgroundColor: Object.values(CHART_COLORS),
+            fill: true,
+            },
+            { 
+            data: updateUniques,
+            backgroundColor: Object.values(CHART_COLORS),
+            fill: true,
+            }]
+        },
+    });
+
+    //Device Stats
+    var deviceLabels = [];
+    var deviceTotals = [];
+    var deviceUniques = [];
+    for (let key in updateReport.topDevices) {
+        if (updateReport.topDevices.hasOwnProperty(key)) {
+            //console.log(key, updateReport.topDevices[key]);
+            deviceLabels.push(updateReport.topDevices[key].deviceString);
+            deviceTotals.push(updateReport.topDevices[key].count);
+            deviceUniques.push(updateReport.topDevices[key].uniqueDevices);
+        }
+    }
+    var ctx = document.getElementById('deviceChart');
+    var myChart = new Chart(ctx, {
+        type: 'pie',
+        options: {
+            responsive: true,
+            plugins: {
+                legend: {
+                    position: 'top',
+                },
+                title: {
+                    display: true,
+                    text: 'Most Active Devices'
+                }
+            }
+        },
+        data: {
+        labels: deviceLabels,
+        datasets: [{ 
+            data: deviceTotals,
+            backgroundColor: Object.values(CHART_COLORS),
+            fill: true,
+            },
+            { 
+            data: deviceUniques,
+            backgroundColor: Object.values(CHART_COLORS),
+            fill: true,
+            }]
+        },
+    });
+
+    //OS Stats
+    var osLabels = [];
+    var osTotals = [];
+    var osUniques = [];
+    for (let key in updateReport.topOSVersions) {
+        if (updateReport.topOSVersions.hasOwnProperty(key)) {
+            //console.log(key, updateReport.topOSVersions[key]);
+            osLabels.push(updateReport.topOSVersions[key].osVersionString);
+            osTotals.push(updateReport.topOSVersions[key].count);
+            osUniques.push(updateReport.topOSVersions[key].uniqueDevices);
+        }
+    }
+    var ctx = document.getElementById('osChart');
+    var myChart = new Chart(ctx, {
+        type: 'pie',
+        options: {
+            responsive: true,
+            plugins: {
+                legend: {
+                    position: 'top',
+                },
+                title: {
+                    display: true,
+                    text: 'Most Active OS Versions'
+                }
+            }
+        },
+        data: {
+        labels: osLabels,
+        datasets: [{ 
+            data: osTotals,
+            backgroundColor: Object.values(CHART_COLORS),
+            fill: true,
+            },
+            { 
+            data: osUniques,
+            backgroundColor: Object.values(CHART_COLORS),
+            fill: true,
+            }]
+        },
+    });
+}

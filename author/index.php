b8c7606fef20d252e951142050cedc87aa2304ea
codepoet80 <jonathan.m.wise@gmail.com>
2022-11-05 08:44:16 -0400

    add author social meta

--- author/index.php
+++ author/index.php
@@ -87,6 +87,30 @@ $homePath = $protocol . $config["service_host"]. "";
 ?>
 <title><?php echo $author_data['author']; ?> - webOS App Museum II</title>
 <link rel="stylesheet" href="<?php echo $protocol . $config["service_host"]; ?>/webmuseum.css">
+<?php
+//Social media meta
+$protocol = ((!emptyempty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";  
+$currurl = $protocol . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];  
+?>
+<meta name="description" content="webOS App Museum II is the definitive historical archive of legacy Palm/HP webOS mobile apps and games!" />
+<link rel="canonical" href="<?php echo $currurl; ?>" />
+<meta property="og:locale" content="en_US" />
+<meta property="og:type" content="website" />
+<meta property="og:title" content="<?php echo $author_data['author']; ?> on webOS App Museum II" />
+<meta property="og:description" content="webOS App Museum II is the definitive historical archive of legacy Palm/HP webOS mobile apps and games!" />
+<meta property="og:url" content="<?php echo $currurl; ?>" />
+<meta property="og:site_name" content="webOS App Museum" />
+<meta property="article:published_time" content="<?php echo date('m/d/Y H:i:s', time()); ?>" />
+<meta property="article:modified_time" content="<?php echo date('m/d/Y H:i:s', time()); ?>" />
+<meta property="og:image" content="https://appcatalog.webosarchive.org/assets/webos-apps.png" />
+<meta property="og:image:width" content="250" />
+<meta property="og:image:height" content="260" />
+<meta property="og:image:type" content="image/png" />
+<meta name="author" content="webOS Archive" />
+<meta name="twitter:card" content="summary" />
+<meta name="twitter:title" content="<?php echo $author_data['author']; ?> on webOS App Museum II" />
+<meta name="twitter:description" content="webOS App Museum II is the definitive historical archive of legacy Palm/HP webOS mobile apps and games!" />
+<meta name="twitter:image" content="<?php echo $use_icon; ?>" />
 </head>
 <body>
 <?php include("../menu.php") ?>

d6eee217b9d8941ad16eedbbb43d44b0f65e1aca
codepoet80 <jonathan.m.wise@gmail.com>
2022-11-05 07:35:05 -0400

    update social meta

--- showMuseumDetails.php
+++ showMuseumDetails.php
@@ -101,23 +101,23 @@ $meta_desc = explode("<br>", $app_detail["description"]);
 $meta_desc = trim($meta_desc[0]);
 ?>
 <title><?php echo $found_app["title"] ?> - webOS App Museum II</title>
-<meta name="description" content="webOS App Museum is the definitive historical archive of legacy Palm/HP webOS mobile apps and games!" />
+<meta name="description" content="webOS App Museum II is the definitive historical archive of legacy Palm/HP webOS mobile apps and games!" />
 <link rel="canonical" href="<?php echo $share_url; ?>" />
 <meta property="og:locale" content="en_US" />
 <meta property="og:type" content="website" />
-<meta property="og:title" content="<?php echo $found_app["title"]; ?>" />
+<meta property="og:title" content="<?php echo $found_app["title"]; ?> on webOS App Museum II" />
 <meta property="og:description" content="<?php echo $meta_desc; ?>" />
 <meta property="og:url" content="<?php echo $share_url; ?>" />
 <meta property="og:site_name" content="webOS App Museum" />
-<meta property="article:published_time" content="2022-05-07T15:10:11+00:00" />
-<meta property="article:modified_time" content="2022-06-28T18:18:31+00:00" />
+<meta property="article:published_time" content="<?php echo $app_detail["lastModifiedTime"]; ?>" />
+<meta property="article:modified_time" content="<?php echo $app_detail["lastModifiedTime"]; ?>" />
 <meta property="og:image" content="<?php echo $use_icon; ?>" />
 <meta property="og:image:width" content="256" />
 <meta property="og:image:height" content="256" />
 <meta property="og:image:type" content="image/png" />
 <meta name="author" content="webOS Archive" />
 <meta name="twitter:card" content="summary" />
-<meta name="twitter:title" content="<?php echo $found_app["title"]; ?>" />
+<meta name="twitter:title" content="<?php echo $found_app["title"]; ?> on webOS App Museum II" />
 <meta name="twitter:description" content="<?php echo $meta_desc; ?>" />
 <meta name="twitter:image" content="<?php echo $use_icon; ?>" />
 <link rel="stylesheet" href="webmuseum.css">

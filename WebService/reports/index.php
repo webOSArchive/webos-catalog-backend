bfbd58c550cc693149ea18835b3f69154f3f23d2
codepoet80 <jon.wise@jonandnic.com>
2023-06-16 14:33:38 -0400

    remove dangling end tag

--- WebService/reports/index.php
+++ WebService/reports/index.php
@@ -13,7 +13,6 @@ $mimeType = "text/html";
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.9.1/chart.min.js" integrity="sha512-ElRFoEQdI5Ht6kZvyzXhYG9NqjtkmlkfYk0wr6wHxU9JEHakS7UJZNeml5ALk+8IKlU6jDgMabC3vkumRokgJA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
         <script src="reports.js"></script>
-        </script>
     </head>
     <body onload="renderReports()">
         <?php include('../../menu.php'); ?>

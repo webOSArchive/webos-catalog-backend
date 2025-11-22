294524f9650448eebbed1ed569a58a022a5b056e
codepoet80 <webosarchive@gmail.com>
2025-11-22 14:46:14 -0500

    smaller text

--- index.php
+++ index.php
@@ -63,6 +63,7 @@ if (strpos($outputObj["filename"], "://") === false) {
     @media all and (max-width: 599px) {
         #hero { padding-top: 0px !important; }
     }
+    small { font-size: smaller; }
   </style>
 </head>
 <body>
@@ -95,7 +96,7 @@ echo file_get_contents("https://www.webosarchive.org/menu.php?docRoot=" . $docRo
     <div id="hero">
       <img src="hero.png" width="480" alt="<?php echo $title ?>" />
       <p><small>Catalog metadata is available on <?php echo "<a href='" . $github . "'>GitHub</a>"?> | <a href="https://appcatalog.webosarchive.org/WebService/reports/">View Stats</a><br>
-      <i>Many items are still missing! If you an old device or personal archive, check the <a href="wanted.txt">wanted</a> <a href="wanted.csv">list</a>, or run the <a href="https://appcatalog.webosarchive.org/app/webOSAppScanner">App Scanner</a> on your device, and <a href="mailto:webosarchive@gmail.com">email us</a> if you have any matches!</i></small></p>
+      <i>Many items are still missing! If you have an old device or personal archive, check the <a href="wanted.txt">wanted</a> <a href="wanted.csv">list</a>, or run the <a href="https://appcatalog.webosarchive.org/app/webOSAppScanner">App Scanner</a> on your device, and <a href="mailto:webosarchive@gmail.com">email us</a> if you have any matches!</i></small></p>
     </div>
   </div>
   <div id="footer">

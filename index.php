a7b1556a183bf564800f9ee5c7c17854939ac99b
codepoet80 <webosarchive@gmail.com>
2025-11-22 14:43:57 -0500

    fix italics

--- index.php
+++ index.php
@@ -94,8 +94,8 @@ echo file_get_contents("https://www.webosarchive.org/menu.php?docRoot=" . $docRo
     </div>
     <div id="hero">
       <img src="hero.png" width="480" alt="<?php echo $title ?>" />
-      <p><i><small>Catalog metadata is available on <?php echo "<a href='" . $github . "'>GitHub</a>"?> | <a href="https://appcatalog.webosarchive.org/WebService/reports/">View Stats</a><br>
-      Many items are still missing! If you an old device or personal archive, check the <a href="wanted.txt">wanted</a> <a href="wanted.csv">list</a>, or run the <a href="https://appcatalog.webosarchive.org/app/webOSAppScanner">App Scanner</a> on your device, and <a href="mailto:webosarchive@gmail.com">email us</a> if you have any matches!</small></i></p>
+      <p><small>Catalog metadata is available on <?php echo "<a href='" . $github . "'>GitHub</a>"?> | <a href="https://appcatalog.webosarchive.org/WebService/reports/">View Stats</a><br>
+      <i>Many items are still missing! If you an old device or personal archive, check the <a href="wanted.txt">wanted</a> <a href="wanted.csv">list</a>, or run the <a href="https://appcatalog.webosarchive.org/app/webOSAppScanner">App Scanner</a> on your device, and <a href="mailto:webosarchive@gmail.com">email us</a> if you have any matches!</i></small></p>
     </div>
   </div>
   <div id="footer">

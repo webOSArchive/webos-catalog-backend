0413b71bbd09bbef1053cb44e927ea90fd9b89c3
codepoet80 <webosarchive@gmail.com>
2025-11-25 10:35:13 -0500

    update footer

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <p style="clear:both;margin-top:60px;" class="legal">
-<a href="http://webosarchive.org">webOS Archive</a> provides this listing without profit and under Fair Use provisions and <a href="https://www.copyright.gov/1201/docs/librarian_statement_01.html">other exemptions</a> of U.S. copyright law, for the purposes of historical preservation.
+<a href="http://webosarchive.org">webOS Archive</a> provides this listing without profit and under Fair Use provisions and <a href="https://www.federalregister.gov/documents/2024/10/28/2024-24563/exemption-to-prohibition-on-circumvention-of-copyright-protection-systems-for-access-control">other exemptions</a> of U.S. copyright law, for the purposes of historical preservation.
 <?php
     if (isset($config['contact_email']) && !empty($config['contact_email'])) {
       echo "If you have questions or comments, or to submit a take down request for IP you own the rights to, please <a href=\"javascript:document.location=atob('" . base64_encode("mailto:" . $config['contact_email'] . "?subject=webOS App Museum") . "')\">email the curator</a>!";
